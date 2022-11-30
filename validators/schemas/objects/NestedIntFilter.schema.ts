02caecc76fb639e03d68be6c64d3bcf070fbf695
Samuel Mutumba Jr <81653788+samutumba@users.noreply.github.com>
2022-11-30 16:20:07 -0500

    Adjusted NestedIntFilterObjectSchema

--- validators/schemas/objects/NestedIntFilter.schema.ts
+++ validators/schemas/objects/NestedIntFilter.schema.ts
@@ -4,6 +4,5 @@ import "../helpers/oneOfSchemas.helper.ts"
 
 
 export const NestedIntFilterObjectSchema = Yup.object({
-    equals: Yup.number(),  in: Yup.array().of(Yup.number()),  notIn: Yup.array().of(Yup.number()),  lt: Yup.number(),  lte: Yup.number(),  gt: Yup.number(),  gte: Yup.number(),  not: Yup.mixed().oneOfSchemas([Yup.number(),
-Yup.lazy(() => NestedIntFilterObjectSchema.default(undefined))])
+    equals: Yup.number(),  in: Yup.array().of(Yup.number()),  notIn: Yup.array().of(Yup.number()),  lt: Yup.number(),  lte: Yup.number(),  gt: Yup.number(),  gte: Yup.number()
 });
